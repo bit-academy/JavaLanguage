c1c0a6c53018cc49301e9fd3a2bd11408c7f756f
Jase HA <mindwing@gmail.com>
2015-07-14 02:53:59 +0900

    Update CharDataType.java

--- CharDataType.java
+++ CharDataType.java
@@ -2,7 +2,7 @@ public class CharDataType {
 	public static void main(String[] args) {
 		char c1 = 'a';
 		char c2 = '\u0061';
-		char c3 = '°¡';
+		char c3 = 'ê°€';
 		char c4 = '\uac00';
 
 		System.out.println(c1 == c2);
