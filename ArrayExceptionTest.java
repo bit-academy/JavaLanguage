3807fd860effaa4d907c51392ec7d97552e22a65
mindwing <mindwing@gmail.com>
2015-07-15 11:24:28 +0900

    Array example

---
+++ ArrayExceptionTest.java
@@ -0,0 +1,15 @@
+class ArrayExceptionTest {
+    public static void main(String[] args) {
+    	int a[] = {1, 2};
+
+	// 
+	// System.out.println(a[-1]);
+	// System.out.printkl(a[2]);
+
+	a = null;
+	
+	//
+	//a[0] = 3;
+    }
+}
+
